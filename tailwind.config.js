8f6060cbb0b9db2f70c1e4050710e16931fac119
Yuuki175 <realouise175@gmail.com>
2023-08-03 16:07:14 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'cream':'#FFFFFF',
+        'grinn':'#288B66',
+        'blakk':'#000000',
+      },
+      breakpoints:{
+
+      }
+    },
+  },
+  plugins: [],
+}
+
